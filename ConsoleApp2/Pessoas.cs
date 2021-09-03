ec9cecbf63b87e34e9af6c0644737f9f6ad2147b
thiagoatf@hotmail.com <thiagoatf@hotmail.com>
2021-09-03 04:10:31 -0300

    Add project files.

---
+++ ConsoleApp2/Pessoas.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Pessoas
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public Pessoas(string nome, int idade)
+        {
+            Nome = nome;
+            Idade = idade;
+        }
+
+        public override string ToString()
+        {
+            return "Pessoa mais velha: " + Nome;
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dados da primeira pessoa: ");
+            Console.Write("Nome: ");
+            string nome1 = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade1 = int.Parse(Console.ReadLine());
+
+            Pessoas n1 = new Pessoas(nome1, idade1);
+
+            Console.WriteLine("Dados da segunda pessoa: ");
+            Console.Write("Nome: ");
+            string nome2 = Console.ReadLine();
+            Console.Write("Idade: ");
+            int idade2 = int.Parse(Console.ReadLine());
+
+            Pessoas n2 = new Pessoas(nome2, idade2);
+
+            Console.WriteLine();
+
+            if (idade1 > idade2)
+            {
+                Console.WriteLine(n1);
+            }
+            else
+            {
+                Console.WriteLine(n2);
+            }
+            
+        }
+    }
+}
